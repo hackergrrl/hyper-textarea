544f6e411da477097b58d203af46e5dcc1db7711
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-07-02 17:40:56 -0400

    handle local insertions

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
+var hstring = require('hyper-string')
+var memdb = require('memdb')
 var getTextOpStream = require('textarea-op-stream')
 
-function wrap (ta) {
+function wrap (ta, db) {
   this.opStream = getTextOpStream(ta)
 
+  var string = hstring(db)
+
+  var self = this
   this.opStream.on('data', function (op) {
-    console.log(op)
+    // TODO: prevent race conditions here!
+    if (op.op === 'insert') {
+      string.chars(function (err, chars) {
+        if (err) throw err
+        var at = null
+        if (op.pos && chars[op.pos - 1]) {
+          at = chars[op.pos - 1].pos
+        }
+        var toInsert = op.str
+        string.insert(at, op.str[0], postInsert)
+        function postInsert (err, elem) {
+          console.log('inserted "' + elem.chr + '" @ ' + at)
+          at = elem.pos
+          toInsert = toInsert.slice(1)
+          if (toInsert.length > 0) {
+            string.insert(at, toInsert[0], postInsert)
+          } else {
+            console.log('DONE')
+            string.text(function (err, text) {
+              console.log('FULL TEXT:', text)
+            })
+          }
+        }
+      })
+    } else if (op.op === 'delete') {
+      // 1. find the ID of the char referred to
+      // 2. make successive async deletes
+    }
   })
 
   return ta
@@ -22,4 +54,4 @@ ta.setAttribute('rows', 24)
 
 document.body.innerHTML = ''
 document.body.appendChild(ta)
-wrap(ta)
+wrap(ta, memdb())
