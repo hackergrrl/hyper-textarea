ad8d55996fc8441a69ce9050d3447868e99a1060
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-10-10 20:05:42 -0400

    concurrency experimentation

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 var hstring = require('hyper-string')
 var getTextOpStream = require('textarea-op-stream')
 var mutexify = require('mutexify')
-var debug = require('debug')('hyper-textarea')
+var debug = console.log //require('debug')('hyper-textarea')
 
 module.exports = function (ta, db, id) {
   id = id || ('' + Math.random()).substring(2, 3)
 
   var opStream = getTextOpStream(ta)
 
-  var lock = mutexify()
+  var refreshLock = mutexify()
+  var pendingOps = []
+
+  var pendingRefresh = false
+
+  var pendingLocalInserts = {}
 
   var string = hstring(db)
 
-  var pendingIndexReady = false
-
   function ready () {
     debug('hyper-string indexer is ready!')
-    pendingIndexReady = false
     refresh()
   }
   string.index.ready(ready)
@@ -37,19 +39,26 @@ module.exports = function (ta, db, id) {
 
     if (remote) {
       debug('refreshing due to remote op', value)
-      // refresh(value.op === 'insert' ? node.key : undefined)
-      if (!pendingIndexReady) {
-        debug('scheduled INDEX')
-        string.index.ready(ready)
-        pendingIndexReady = true
-      } else {
-        debug('skipped scheduling INDEX')
-      }
+      refresh(value.op === 'insert' ? node.key : undefined)
+      // if (!pendingIndexReady) {
+      //   debug('scheduled INDEX')
+      //   string.index.ready(ready)
+      //   pendingIndexReady = true
+      // } else {
+      //   debug('skipped scheduling INDEX')
+      // }
     }
   })
 
   function refresh (insertedAt) {
-    lock(function (release) {
+    if (pendingOps.length > 0) {
+      return
+    }
+    if (pendingRefresh) {
+      return
+    }
+    pendingRefresh = true
+    // refreshLock(function (release) {
       var isFocused = document.activeElement === ta
       var offset = 0
 
@@ -73,51 +82,66 @@ module.exports = function (ta, db, id) {
           ta.selectionStart = start + offset
           ta.selectionEnd = end + offset
         }
-        release()
+        pendingRefresh = false
+        // release()
       })
-    })
+    // })
   }
 
   opStream.on('data', function (op) {
-    // TODO: prevent race conditions here!
-
-    var start = new Date().getTime()
-    lock(function (release) {
-      debug(id, 'got an op:', op)
-      if (op.op === 'insert') {
-        string.chars(function (err, chars) {
-          if (err) throw err
-          var at = null
-          if (op.pos && chars[op.pos - 1]) {
-            at = chars[op.pos - 1].pos
-          } else {
-            debug('FYI: inserting op (' + op + ') at pos NULL')
-          }
-
-          debug('inserting: ', op)
-          string.insert(at, op.str, function (err, ops) {
-            if (err) throw err
-            release()
-          })
-        })
-      } else if (op.op === 'delete') {
-        string.chars(function (err, chars) {
-          if (err) throw err
-          if (!chars[op.pos]) {
-            throw new Error('deletion location doesn\'t exist locally')
-          }
-
-          var at = chars[op.pos].pos
-
-          debug('deleting: ', op)
-          string.delete(at, op.count, function (err, ops) {
-            if (err) throw err
-            release()
-          })
-        })
-      }
-    })
+    debug(id, 'got an op:', op)
+    if (pendingOps.length > 0) {
+      pendingOps.push(op)
+      return
+    } else {
+      pendingOps.push(op)
+      processOpQueue()
+    }
   })
+
+  function processOpQueue () {
+    if (pendingOps.length === 0) {
+      return
+    }
+    var op = pendingOps[0]
+
+    if (op.op === 'insert') {
+      string.chars(function (err, chars) {
+        if (err) throw err
+        var at = null
+        if (op.pos && chars[op.pos - 1]) {
+          at = chars[op.pos - 1].pos
+        } else {
+          debug('FYI: inserting op (' + op + ') at pos NULL')
+        }
+
+        debug('inserting: ', op)
+
+        string.insert(at, op.str, function (err, ops) {
+          if (err) throw err
+          pendingOps.shift()
+          processOpQueue()
+        })
+      })
+    } else if (op.op === 'delete') {
+      string.chars(function (err, chars) {
+        if (err) throw err
+        if (!chars[op.pos]) {
+          throw new Error('deletion location doesn\'t exist locally')
+        }
+
+        var at = chars[op.pos].pos
+
+        debug('deleting: ', op)
+
+        string.delete(at, op.count, function (err, ops) {
+          if (err) throw err
+          pendingOps.shift()
+          processOpQueue()
+        })
+      })
+    }
+  }
 
   return string
 }
