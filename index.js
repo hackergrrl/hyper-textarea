9571907ce86f46b2aa881374adf9fd7088a6f4e4
Stephen Whitmore <sww@eight45.net>
2017-01-27 21:16:02 -0800

    big refactor

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var getTextOpStream = require('textarea-op-stream')
-var mutexify = require('mutexify')
 var debug = console.log //require('debug')('hyper-textarea')
 
 module.exports = function (ta, string, id) {
@@ -7,18 +6,14 @@ module.exports = function (ta, string, id) {
 
   var opStream = getTextOpStream(ta)
 
-  var refreshLock = mutexify()
-  var pendingOps = []
+  var pendingLocalOps = []
 
   var pendingRefresh = false
+  var pendingCursorOffset = 0
 
-  var pendingLocalInserts = {}
+  var cache = null
 
-  function ready () {
-    debug('hyper-string indexer is ready!')
-    refresh()
-  }
-  string.index.ready(ready)
+  string.index.ready(refresh)
 
   string.log.on('add', function (node) {
     var value = node.value
@@ -34,71 +29,80 @@ module.exports = function (ta, string, id) {
 
     debug('hyper-string gave us a ' + (remote ? 'remote' : 'local') + ' "add":', node)
 
-    if (remote) {
+    if (remote && pendingLocalOps.length === 0) {
       debug('refreshing due to remote op', value)
-      refresh(value.op === 'insert' ? node.key : undefined)
+
+      if (value.op === 'insert') {
+        var offset = computeNeededCursorOffset(ta.selectionStart, node.key, cache.chars)
+        pendingCursorOffset += offset
+      }
+
+      refresh()
     }
   })
 
   function refresh (insertedAt) {
-    if (pendingOps.length > 0) {
-      return
-    }
-    if (pendingRefresh) {
+    if (pendingLocalOps.length > 0 || pendingRefresh) {
       return
     }
     pendingRefresh = true
-    // refreshLock(function (release) {
+
+    refreshCache(function (err, cache) {
+      if (err) throw err
+
+      pendingRefresh = false
+
+      if (pendingLocalOps.length > 0) return
+
+      var chars = cache.chars
+      var text = cache.text
+      var start = ta.selectionStart
+      var end = ta.selectionEnd
+
+      debug(id, 'REFRESH to', text)
+      ta.value = text
+
+      // If the textarea is focused, ensure its cursor position is maintained
       var isFocused = document.activeElement === ta
-      var offset = 0
-
-      string.chars(function (err, chars) {
-        if (err) throw err
-        var text = chars.map(function (c) { return c.chr }).join('')
-
-        for (var i = 0; i < chars.length; i++) {
-          if (i > ta.selectionStart) break
-          if (chars[i].pos === insertedAt) { offset++; break }
-        }
-
-        debug(id, 'REFRESH to', text)
-        var start = ta.selectionStart
-        var end = ta.selectionEnd
-        ta.value = text
-
-        // If the textarea is focused, ensure its cursor position is maintained
-        if (isFocused) {
-          ta.dispatchEvent(new Event('focus'))
-          ta.selectionStart = start + offset
-          ta.selectionEnd = end + offset
-        }
-        pendingRefresh = false
-        // release()
-      })
-    // })
+      if (isFocused) {
+        ta.dispatchEvent(new Event('focus'))
+      }
+      ta.selectionStart = start + pendingCursorOffset
+      ta.selectionEnd = end + pendingCursorOffset
+      pendingCursorOffset = 0
+    })
   }
 
   opStream.on('data', function (op) {
     debug(id, 'got an op:', op)
-    if (pendingOps.length > 0) {
-      pendingOps.push(op)
-      return
-    } else {
-      pendingOps.push(op)
+
+    pendingLocalOps.push(op)
+
+    if (pendingLocalOps.length === 1) {
       processOpQueue()
     }
   })
 
   function processOpQueue () {
-    if (pendingOps.length === 0) {
+    if (pendingLocalOps.length === 0) {
+      refresh()
       return
     }
-    var op = pendingOps[0]
+    var op = pendingLocalOps[0]
 
-    if (op.op === 'insert') {
-      string.chars(function (err, chars) {
-        if (err) throw err
+    if (!cache) {
+      // TODO: can prob drop this branch
+      console.log('refreshing cache before doing local op..')
+      refreshCache(performOp)
+    } else {
+      performOp()
+    }
+
+    function performOp () {
+      var chars = cache.chars
+      if (op.op === 'insert') {
         var at = null
+        console.log('insert', op.pos, chars)
         if (op.pos && chars[op.pos - 1]) {
           at = chars[op.pos - 1].pos
         } else {
@@ -109,29 +113,68 @@ module.exports = function (ta, string, id) {
 
         string.insert(at, op.str, function (err, ops) {
           if (err) throw err
-          pendingOps.shift()
+          pendingLocalOps.shift()
+
+          // update the cache
+          cache.text = splice(cache.text, op.pos, 0, op.str)
+          var newChars = ops.map(function (op, idx) {
+            return {
+              chr: op.str[idx],
+              pos: op.pos
+            }
+          })
+          cache.chars.splice.apply(this, [op.pos, 0].concat(newChars))
+
           processOpQueue()
         })
-      })
-    } else if (op.op === 'delete') {
-      string.chars(function (err, chars) {
-        if (err) throw err
+      } else if (op.op === 'delete') {
         if (!chars[op.pos]) {
           throw new Error('deletion location doesn\'t exist locally')
         }
-
         var at = chars[op.pos].pos
 
         debug('deleting: ', op)
 
         string.delete(at, op.count, function (err, ops) {
           if (err) throw err
-          pendingOps.shift()
+          pendingLocalOps.shift()
+
+          // update the cache
+          cache.text = splice(cache.text, op.pos, op.count, '')
+          cache.chars.splice(op.pos, op.count)
+
           processOpQueue()
         })
-      })
+      } else {
+        throw new Error('uknown op type (' + op.op + ')')
+      }
     }
+  }
+
+  function refreshCache (done) {
+    string.chars(function (err, chars) {
+      if (err) return done(err)
+
+      cache = {
+        chars: chars,
+        text: chars.map(function (c) { return c.chr }).join('')
+      }
+
+      done(null, cache)
+    })
   }
 
   return string
 }
+
+function computeNeededCursorOffset (selectionStart, insertedAt, chars) {
+  for (var i = 0; i < chars.length; i++) {
+    if (i > selectionStart) return 0
+    if (chars[i].pos === insertedAt) return 1
+  }
+}
+
+function splice (text, start, delCount, toAdd) {
+  return text.substring(0, start) + toAdd + text.substring(start + delCount)
+}
+
